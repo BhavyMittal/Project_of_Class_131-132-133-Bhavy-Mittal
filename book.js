67f573caec70efc8f79162725b47afb97e2d3fc0
BhavyMittal <98758245+BhavyMittal@users.noreply.github.com>
2023-01-06 23:26:04 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,51 @@
+Status = "";
+Book_image = "";
+objects = [];
+
+function preload(){
+    Book_image = loadImage("Book.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(Book_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(Book_image,0,0,640,350);
+
+    if(Status != ""){
+
+        for(i = 0; i < objects.length; i++){
+
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("no_of_objects_detected").innerHTML = "No of objects detected are :" + objects.length;
+    
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+
+             text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+            
+            noFill();
+            stroke("#fc0303");
+             rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
+}
